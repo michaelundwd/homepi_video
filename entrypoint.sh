693c353b42b5c66504e260a606d0e1251ddb5e05
michaelundwd <45148992+michaelundwd@users.noreply.github.com>
2025-09-01 19:52:26 +0100

    tidy up entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # entrypoint.sh
-# runs the startup script in the host that copies the daily video folder tidy up schedule to the container at startup and runs the video move routines
-# last modified 01/09/2025
+# Runs the startup script in the host that copies the daily video folder tidy up schedule to the container at startup and runs the video move routines
+# Last modified 01/09/2025
 exec ./homepi_opt/video/startup-script.sh
 #
