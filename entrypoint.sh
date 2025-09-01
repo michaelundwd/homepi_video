c9d7bf8d5be5ae38b532157e5c6c86cf2f26a197
michaelundwd <45148992+michaelundwd@users.noreply.github.com>
2025-09-01 11:22:47 +0100

    Update entrypoint.sh
    renamed starting script to startup

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # entrypoint.sh
-# runs the reargarden script in the host that copies the daily video folder tidy up schedule to the container at startup and runs the video move routines
-# the next line needs modification for the video actions
-exec ./homepi_opt/video/reargarden-script.sh
+# runs the startup script in the host that copies the daily video folder tidy up schedule to the container at startup and runs the video move routines
+# last modified 01/09/2025
+exec ./homepi_opt/video/startup-script.sh
 #
