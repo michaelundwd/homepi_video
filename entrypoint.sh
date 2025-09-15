0a2884ac90c3ca96f2f37e56ed78d77c37685db5
michaelundwd <45148992+michaelundwd@users.noreply.github.com>
2025-09-15 10:16:18 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@
 # Runs the startup script to run the video move routines  #
 # Last modified 14/09/2025                                #
 ###########################################################
-exec /homepi_opt/video/startup-script.sh
+exec /homepi_opt/video/startup.sh
 #
